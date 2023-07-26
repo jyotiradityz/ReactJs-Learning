4209f45c352560bfec9144a23b75dd143069a890
jyotiradityz <patiljyotiraditya123@gmail.com>
2023-07-26 11:34:14 +0530

    folder struct

---
+++ react/src/Pokemon.js
@@ -0,0 +1,17 @@
+const Pokemon = (probes) => {
+    const {img,name,ptype,details,number}=probes;
+    const displaytitle=()=>{
+      console.log(probes.name);
+    }
+    return (
+      <section className='pokemon'>
+        <img src={img}></img><br />
+        <h2 className="name">{name}</h2>
+        <h3 className="type">Type:{ptype}</h3>
+        <h4 className="details">{details} </h4>
+        <span className="number">{`#${number+1}`}</span>
+        <button className="disbnt" >Add to wishlist</button>
+      </section>
+    )
+}
+export default Pokemon;
---
+++ react/src/pokemonlist.js
@@ -0,0 +1,68 @@
+import mew from './Images/MEW.jpeg'
+export const pokemonlist = [
+    {
+      img: 'https://www.xtrafondos.com/wallpapers/vertical/greninja-ash-contra-mega-charizard-4981.jpg',
+      name: 'Mega Charizard-X',
+      ptype: 'Fire and Dragon',
+      details: 'A Verystrong Pokemon evolve from Charizard by mega stone-X',
+      id:1,
+    },
+    {
+      img: 'https://w0.peakpx.com/wallpaper/290/941/HD-wallpaper-gardevoir-pokemon-pokemon.jpg',
+      name: 'Gardevoir',
+      ptype: 'Psychic and Fairy',
+      details: 'A Beautifull Pokemon evolve by kirla, has very different types of moves',
+      id:2,
+    },
+    {
+      img: 'https://w.forfun.com/fetch/ec/eca95a910ac23e3fd5a8fcf0680b95a6.jpeg',
+      name: 'Pickachu',
+      ptype: 'Electirc',
+      details: 'An electric mouse tail Pokemon which is too famous in pokefans',
+      id:3,
+    },
+    {
+      img: 'https://wallpaperaccess.com/full/2834681.jpg',
+      name: 'Solgaleo',
+      ptype: 'Steel and Psychic',
+      details: 'An Ultra world pokemon representing sun',
+      id:4,
+    },
+    {
+      img: 'https://w0.peakpx.com/wallpaper/1011/359/HD-wallpaper-lunala-anime-pokemon-pokemonmoon-pokemonsunandmoon.jpg',
+      name: 'Mewtwo',
+      ptype: 'Ghost and Psychic',
+      details: 'An Ultra world pokemon representing moon',
+      id:5,
+    },
+    {
+      img: 'https://i.pinimg.com/originals/48/6c/69/486c6978e9fd27aa398ce293b050d493.png',
+      name: 'Necrozma',
+      ptype: 'Psychic',
+      details: 'Ultra world pokemon which is powerfull than other ulta-beasts',
+      id:6,
+    },
+    {
+      img: mew,
+      name: 'Mew',
+      ptype: 'Psychic',
+      details: 'The ancient pokemon has DNA of mostly every pokemon, has many attacks, one of the cutest pokemoon',
+      id:7,
+    },
+    
+    {
+      img: 'https://wallpapercave.com/wp/wp6011576.jpg',
+      name: 'Mega Rayquaza  ',
+      ptype: 'Dragon and Flying',
+      details: 'The most powerfull in weather trio, evolve by the mega stone',
+      id:8,
+    },
+    {
+      img: 'https://e0.pxfuel.com/wallpapers/33/757/desktop-wallpaper-latios-und-latias-sektor-8.jpg',
+      name: 'Latios and Latias',
+      ptype: 'Psychic and Dragon',
+      details: 'The red Latias is female, while the blue Latios is male. By folding in their arms, they can fly faster than fighter jets.',
+      id:9,
+    }
+    
+];
