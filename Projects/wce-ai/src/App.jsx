e93c509aed60f00c1cf9e1e235bc505f94e1c13f
Jyotiraditya Patil <patiljyotiraditya123@gmail.com>
2023-09-05 15:20:24 +0530

    "Projects Started"

---
+++ Projects/wce-ai/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <div>AI-3</div>
+}
+
+export default App
